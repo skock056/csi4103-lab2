096cd356f10d5ed79627b0de1080ba82d3dc8905
skock056 <skock056@uottawa.ca>
2023-01-24 03:53:11 -0500

    Exercise 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,47 @@
+
+// EXERCISE 1.2
+const Header = (header) => {
+  return <h1>{header.course}</h1>;
+};
+
+const Part = (content) => {
+  return (
+    <p>
+      {content.part} {content.exercise}
+    </p>
+  );
+};
+
+const Content = (content) => {
+  return (
+    <div>
+      <Part
+        part={content.parts[0].name}
+        exercise={content.parts[0].exercises}
+      />
+      <Part
+        part={content.parts[1].name}
+        exercise={content.parts[1].exercises}
+      />
+      <Part
+        part={content.parts[2].name}
+        exercise={content.parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+const Total = (total) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {total.parts[0].exercises +
+        total.parts[1].exercises +
+        total.parts[2].exercises}
+    </p>
+  );
+};
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
