36bb7f77a08213e86b92565ff856fd2bdafcc5e9
skock056 <skock056@uottawa.ca>
2023-01-24 03:49:33 -0500

    Exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,23 +7,19 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 // EXERCISE 1.1
 import React from 'react'
 import ReactDOM from 'react-dom/client'
